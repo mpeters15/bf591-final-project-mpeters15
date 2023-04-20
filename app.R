b1f3228e8389b63fd3a4c83cf49c761f02c217f0
Michael Peters <mppeters@scc2.bu.edu>
2023-04-20 13:57:13 -0400

    Add app.R

---
+++ app.R
@@ -0,0 +1,19 @@
+library(shiny)
+library(ggplot2)
+library(colourpicker)
+library(dplyr)
+library(tidyverse)
+library(ggrepel)
+library(DT)
+library(tidyverse)
+library(ComplexHeatmap)
+library(beeswarm)
+library(ggbeeswarm)
+library(RColorBrewer)
+
+options(shiny.maxRequestSize = 30 * 1024 ^ 2)
+
+source("src/ui.R")
+source("src/server.R")
+
+shinyApp(ui = ui, server = server)
