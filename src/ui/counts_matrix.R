ee05893f1cd81b31fb47c339cde9d48713cb1b39
Michael Peters <mppeters@scc2.bu.edu>
2023-04-25 21:37:38 -0400

    Add Counts Matrix UI

--- src/ui/counts_matrix.R
+++ src/ui/counts_matrix.R
@@ -1,2 +1,82 @@
 counts_matrix <- tabPanel("Counts Matrix",
-)
+                          br(),
+                          sidebarLayout(
+                            sidebarPanel(
+                              fileInput(
+                                "p2_fileinput_normalized_counts_matrix",
+                                "Load normalized counts matrix:",
+                                accept = ".csv",
+                                placeholder = "normalized_counts_matrix.csv",
+                              ),
+                              sliderInput(
+                                "p2_slider_percentile_variance",
+                                "Select genes with at least X percentile of variance:",
+                                min = 0,
+                                max = 100,
+                                value = 50,
+                                step = 1,
+                              ),
+                              sliderInput(
+                                "p2_slider_nonzero_gene_samples",
+                                "Select genes with at least X samples that are non-zero:",
+                                min = 1,
+                                max = 2,
+                                value = 2,
+                                step = 1,
+                              ),
+                              submitButton("Run", width = '100%'),
+                            ),
+                            mainPanel(
+                              tabsetPanel(
+                                tabPanel(
+                                  "Post-Filtering Summary",
+                                  br(),
+                                  htmlOutput("p2_htmloutput_sample_size"),
+                                  dataTableOutput("p2_table"),
+                                ),
+                                tabPanel(
+                                  "Diagnostic Scatter Plots",
+                                  br(),
+                                  mainPanel(
+                                    plotOutput("p2_plotoutput_median_count_vs_variance"),
+                                    br(),
+                                    plotOutput("p2_plotoutput_median_count_vs_number_of_zeros"),
+                                  ),
+                                ),
+                                tabPanel(
+                                  "Clustered Heatmap",
+                                  br(),
+                                  mainPanel(
+                                    markdown("Clustered Heatmap of Counts Post-Filtering"),
+                                    plotOutput("p2_heatmap"), 
+                                  ),
+                                  # ),
+                                ),
+                                tabPanel(
+                                  "PCA Projections Scatter Plot",
+                                  br(),
+                                  sidebarLayout(
+                                    sidebarPanel(
+                                      radioButtons(
+                                        "p2_radio_principal_component_1",
+                                        "Select Principal Component (X):",
+                                        choices = 1:10,
+                                        selected = 1,
+                                      ),
+                                      radioButtons(
+                                        "p2_radio_principal_component_2",
+                                        "Select Principal Component (Y):",
+                                        choices = 1:10,
+                                        selected = 2,
+                                      ),
+                                      markdown("Click Run to re-generate the plot."),
+                                    ),
+                                    mainPanel(
+                                      plotOutput("p2_scatter_pca"),
+                                    ),
+                                  ),
+                                ),
+                              ),
+                            ),
+                          ),
+                        )
